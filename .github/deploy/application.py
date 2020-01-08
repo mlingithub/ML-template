c4d9caa04bd8d20f2fc593d4c47129b0acae7ed9
Ahmed Bilal <ahbilal@microsoft.com>
2020-01-08 12:09:45 -0800

    update

--- .github/deploy/application.py
+++ .github/deploy/application.py
@@ -1,39 +1,57 @@
-from flask import Flask, render_template, request, jsonify, flash, url_for, request, redirect
+from flask import Flask, render_template, request, jsonify, flash, url_for, request, redirect,session
 import numpy as np
 import pickle as p
 import json
-# sample deplyment file
+#from keras.preprocessing.text import Tokenizer
+#from keras.preprocessing.sequence import pad_sequences
+from scipy.sparse import hstack
+
+# adaw
 
 app = Flask(__name__)
 
-modelfile = 'model.pickle'
+
+modelfile = 'toxic.pickle'
 model = p.load(open(modelfile, 'rb'))
+#model._make_predict_function()
+with open('word_vectorizer.pickle', 'rb') as handle:
+    word_vectorizer = p.load(handle)
+with open('char_vectorizer.pickle', 'rb') as handle:
+    char_vectorizer = p.load(handle)
+
+# maxlen = 100
 
 
-@app.route("/predict/", methods=['post','get'])
+@app.route("/", methods=['post','get'])
 def makecalc():
-    message = 'hey'
+    message=""
+    color="black"
 
     if request.method == 'POST':
-        age = request.form.get('age')  # access the data inside
-        message=age
 
-        data = [[1, 1, int(age), 1, 1, 100.25]]
-        prediction = np.array2string(model.predict(data))
-        message=str(prediction)
-        print(age)
+        stimulus = [request.form.get('textInput')]  # access the data inside
 
-    return render_template('predict.html', message=message)
+        word_features = word_vectorizer.transform(stimulus)
+        char_features = char_vectorizer.transform(stimulus)
+        features = hstack([char_features, word_features])
+        #list_tokenized_text = tokenizer.texts_to_sequences([age])
+        #X_text = pad_sequences(list_tokenized_text, maxlen=maxlen)
+        
+        prediction = model.predict(features)
+        if prediction[0]==1:
+            message= "Toxic"
+            color="red"
+        else:
+            message= "Not Toxic"
+
+    return render_template('predict.html', message=message, color=color)
 
 @app.route('/api/', methods=['post'])
 def makecalc2():
-    #modelfile = '.artifacts/model.pickle'
-    #model = p.load(open(modelfile, 'rb'))
-    #data = [[1, 1, 2, 1, 1, 100.25]]
     data = request.get_json()
     prediction = np.array2string(model.predict(data))
 
     return jsonify(prediction)
 
 if __name__ == '__main__':
-    app.run()
+    app.run(threaded=False)
